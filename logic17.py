8b0a2bd06d5d0e8d5fcbb8ef194e98105d536a40
sobirismatov <name@uzcoder96@gmail.com>
2022-11-01 09:57:26 +0500

    17

--- logic17.py
+++ logic17.py
@@ -11,5 +11,5 @@ def main(a):
     x3=a//100%10
     x4=a//10%10
     x5=a%10
-    return x5>=x4 and x4>=x3 and x3>=x2 and x2>=x1 or x5<=x4 and x4<=x3 and x3<=x2 and x2<=x1  
+    return  x5<=x4 and x4<=x3 and x3<=x2 and x2<=x1  
 
